c2af4dd2c706f5dd6535bc2fba26602358c96178
AISHU5425 <aishu.hgm@gmail.com>
2025-11-21 15:45:48 +0530

    Add files via upload

--- fn_calls.c
+++ fn_calls.c
@@ -13,6 +13,8 @@ void main()
 {
      int a=90;
      int b=89;
+     printf("call by value:\n");
      sum_value(a,b);
-    sum_ref(&a,&b);
+     printf("call by value:\n");
+     sum_ref(&a,&b);
 }
