2fa9ce44be8b76b095fbc9420fa9bb94c23c4740
AISHU5425 <aishu.hgm@gmail.com>
2025-11-21 10:47:32 +0530

    Add files via upload

---
+++ scrreen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void names (char first[], char last[])
+{
+    int i=0;
+    char full[100];
+    while (first[i]!='\0')
+    {
+        full[i]=first[i];
+        i++;
+    }
+    full[i]=' ';
+    i++;
+    int j=0;
+    while (last[j]!='\0')
+    {
+        full[i]=last[j];
+        i++;
+        j++;
+    }
+    full [i]='\0';
+    printf("%s",full);
+}
+void main()
+{    
+    int i;
+    int sw;
+    int len=i+1;
+    if (len>sw)
+    printf("fits in  the screen");
+    else
+    printf("does not fit in the screen!");
+    char full[100]="raj";
+    char last[100]="kumar";
+    names (full,last);
+}
